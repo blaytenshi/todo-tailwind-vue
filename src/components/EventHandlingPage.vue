0a4a4a7095a5d05e398c4260e52351124b9b6b58
Jimmy Tang <blay.tenshi@gmail.com>
2020-01-20 10:47:08 +1100

    Added Custom Component and extracting Tailwind Classes!

--- src/components/EventHandlingPage.vue
+++ src/components/EventHandlingPage.vue
@@ -1,17 +1,33 @@
 <template>
     <div id="example-1">
-        <button class="" @click="counter += 1">Add 1</button>
+        <button class="btn-blue" @click="counter += 1">Add 1</button>
         <p>The button above has been clicked {{ counter }} times.</p>
+        <MyComponent testProp="Hi There" />
     </div>
 </template>
 
 <script>
+    import MyComponent from './MyComponent';
+
     export default {
         name: 'EventHandlingPage',
+        components: {
+            MyComponent
+        },
         data: function() {
             return {
                 counter: 0
             }
-        }
+        },
     }
 </script>
+
+<style scoped>
+    .btn-blue {
+        @apply border-blue-700 text-white py-2 px-4 rounded-lg bg-blue-500;
+    }
+
+    .btn-blue:hover {
+        @apply bg-blue-700
+    }
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>My Template</div>
+        <div>{{ testProp }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyComponent',
+        props: {
+            testProp: String
+        }
+    }
+</script>
