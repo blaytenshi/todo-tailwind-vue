735507fcfc35d126445b39377e91e1ca6ee57626
Jimmy Tang <blay.tenshi@gmail.com>
2020-01-21 16:12:11 +1100

    Figured out how V-Model works... finally

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -2,8 +2,8 @@
     <!-- in a custom component, the @input will call the supplied method with an event object -->
     <!-- it doesn't need a :value binding unless you wanna display the value inside the component too -->
     <div>
-        <input class="input mx-1" @input="customInputChanged" />
-        <p class="mx-1">Inside custom component: </p>
+        <input class="input mx-1" :placeholder="placeholder" :value="value" @input="customInputChanged" />
+        <p class="mx-1">Inside custom component: {{ capitalizeThis }}</p>
     </div>
 </template>
 
@@ -11,7 +11,16 @@
     export default {
         name: "MyInput",
         props: {
-            myValue: String
+            value: String,
+            placeholder: String,
+        },
+        data: function() {
+            console.log("data", this.value);
+            console.log("data", this.placeholder);
+
+            return {
+                updatedValue: this.value
+            }
         },
         methods: {
             customInputChanged: function(event) {
@@ -19,6 +28,12 @@
                 this.$emit('inputChanged', event.target.value);
             }
         },
+        computed: {
+            capitalizeThis: function() {
+                console.log("myValue:", this.value);
+                return this.value ? this.value[0].toUpperCase() + this.value.slice(1) : ''
+            }
+        }
     }
 </script>
 
